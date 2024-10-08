a45990ed4ed03cc386b9c4a86113eb5f86bdade7
Garcia <dong.yy1916@gmail.com>
2024-10-08 10:57:35 +0800

    feat: erform

--- worker.js
+++ worker.js
@@ -1,72 +1,52 @@
-// Cloudflare Worker example for Telegram bot integration
+// Constants
 const CHANNEL_ID = "-1002227026093";
-
-// Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual Telegram bot token
 const TELEGRAM_BOT_TOKEN = "7329448395:AAGV_Mf_03RpSXhKqpFyRrPgMkhVWPYpvaI";
-
-// # PRODUCT HUNT API KEYS
 const PRODUCT_HUNT_ACCESS_TOKEN = "76mPN-WTUPaXFRUK_X7Gq7ga1cqMQzJwe1xyPbn7FGI";
+const API_URL = "https://api.producthunt.com/v2/api/graphql";
 
-const queryTemplate = `
-    query GetPosts($postedAfter: DateTime!, $postedBefore: DateTime, $featured: Boolean!, $first: Int!) {
-      posts(order: VOTES, postedAfter:  $postedAfter, postedBefore: $postedBefore, featured: $featured, first: $first) {
-        nodes {
-          id
-          name
-          tagline
-          description
-          votesCount
-          createdAt
-          featuredAt
-          website
+// GraphQL query
+const QUERY_TEMPLATE = `
+  query GetPosts($postedAfter: DateTime!, $postedBefore: DateTime, $featured: Boolean!, $first: Int!) {
+    posts(order: VOTES, postedAfter:  $postedAfter, postedBefore: $postedBefore, featured: $featured, first: $first) {
+      nodes {
+        id
+        name
+        tagline
+        description
+        votesCount
+        createdAt
+        featuredAt
+        website
+        url
+        slug
+        media {
           url
-          slug
-          media {
+        }
+        topics {
+          nodes {
+            id
+            name
+            description
+            slug
             url
-          }
-          topics {
-            nodes {
-              id
-              name
-              description
-              slug
-              url
-            }
           }
         }
       }
     }
-    `;
-
-const api = "https://api.producthunt.com/v2/api/graphql";
-
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
-
-addEventListener("scheduled", async (event) => {
-  const cron = event.cron;
-  switch (cron) {
-    case "0 1 * * *":
-      // Every day at midnight
-      await sendToUser(CHANNEL_ID, "yesterday");
-      break;
-    case "0 0 * * mon":
-      // Every Monday
-      await sendToUser(CHANNEL_ID, "lastweek");
-      break;
-    case "0 0 1 * *":
-      // Every first day of the month
-      await sendToUser(CHANNEL_ID, "lastmonth");
-      break;
-    default:
-      await sendToUser(CHANNEL_ID, "yesterday");
-      break;
   }
+`;
 
-  return new Response(cron + " OK", { status: 200 });
-});
+// Enums
+const TimePeriodEnum = {
+  TODAY: "today",
+  YESTERDAY: "yesterday",
+  THIS_WEEK: "thisweek",
+  THIS_MONTH: "thismonth",
+  LAST_WEEK: "lastweek",
+  LAST_MONTH: "lastmonth",
+};
 
+// Classes
 class Product {
   id;
   name;
@@ -108,27 +88,7 @@ class Product {
   }
 }
 
-// Set up webhook URL.
-// https://api.telegram.org/bot7329448395:AAGV_Mf_03RpSXhKqpFyRrPgMkhVWPYpvaI/setWebhook?url=https://testbot.cain-wuyi.workers.dev/
-
-// delete webhook URL.
-// https://api.telegram.org/bot7329448395:AAGV_Mf_03RpSXhKqpFyRrPgMkhVWPYpvaI/deleteWebhook
-
-// get webhook info.
-// https://api.telegram.org/bot7329448395:AAGV_Mf_03RpSXhKqpFyRrPgMkhVWPYpvaI/getWebhookInfo
-
-// get updates.
-// https://api.telegram.org/bot7329448395:AAGV_Mf_03RpSXhKqpFyRrPgMkhVWPYpvaI/getUpdates
-
-const TimePeriodEnum = {
-  TODAY: "today",
-  YESTERDAY: "yesterday",
-  THIS_WEEK: "thisweek",
-  THIS_MONTH: "thismonth",
-  LAST_WEEK: "lastweek",
-  LAST_MONTH: "lastmonth",
-};
-
+// Helper functions
 function generateQueryForTimePeriod(timePeriod) {
   let today = new Date();
   let postedAfter, postedBefore;
@@ -248,9 +208,26 @@ function generateQueryForTimePeriod(timePeriod) {
     postedBefore,
   };
 }
-async function fetchProductHuntData(chatId, timePeriod) {
-  const { postedAfter, postedBefore } = generateQueryForTimePeriod(timePeriod);
 
+function generateProductHTML(product, index) {
+  return `
+<b>Rank: ${index + 1}   </b>
+
+<b>${product.name}</b> --- <i>${product.tagline}</i>
+
+<b>Product description: </b> ${product.description}
+
+<b>Votes:</b> ${product.votesCount} 
+
+<b>Publish date:</b> ${product.createdAt}
+
+<b>🏷 ${product.topics}</b>
+`;
+}
+
+// API functions
+async function fetchProductHuntData(timePeriod) {
+  const { postedAfter, postedBefore } = generateQueryForTimePeriod(timePeriod);
   const variables = {
     postedAfter,
     postedBefore,
@@ -262,33 +239,7 @@ async function fetchProductHuntData(chatId, timePeriod) {
     delete variables.postedBefore;
   }
 
-  let message;
-  switch (timePeriod) {
-    case TimePeriodEnum.TODAY:
-      message = "Today's products on Product Hunt:";
-      break;
-    case TimePeriodEnum.YESTERDAY:
-      message = "Yesterday's products on Product Hunt:";
-      break;
-    case TimePeriodEnum.THIS_WEEK:
-      message = "This week's products on Product Hunt:";
-      break;
-    case TimePeriodEnum.THIS_MONTH:
-      message = "This month's products on Product Hunt:";
-      break;
-    case TimePeriodEnum.LAST_WEEK:
-      message = "Last week's products on Product Hunt:";
-      break;
-    case TimePeriodEnum.LAST_MONTH:
-      message = "Last month's products on Product Hunt:";
-      break;
-    default:
-      throw new Error("Invalid time period");
-  }
-
-  await sendTelegramResponse(chatId, message);
-
-  const response = await fetch(api, {
+  const response = await fetch(API_URL, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${PRODUCT_HUNT_ACCESS_TOKEN}`,
@@ -296,40 +247,17 @@ async function fetchProductHuntData(chatId, timePeriod) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      query: queryTemplate,
+      query: QUERY_TEMPLATE,
       variables: variables,
     }),
   });
 
   if (!response.ok) {
-    throw new Error(
-      `Failed to fetch data from Product Hunt: ${
-        response.status
-      } ${await response.text()}`
-    );
+    throw new Error(`Failed to fetch data from Product Hunt: ${response.status} ${await response.text()}`);
   }
 
   const data = await response.json();
-  const posts = data.data.posts.nodes;
-  return posts.map((post) => new Product(post));
-}
-
-function generateProductHTML(product, index) {
-  return `
-<b>Rank: ${index + 1}   </b>
-
-<b>${product.name}</b> --- <i>${product.tagline}</i>  <a href="${
-    product.website
-  }">Link</a>
-
-<b>Product description: </b> ${product.description}
-
-<b>Votes:</b> ${product.votesCount} 
-
-<b>Publish date:</b> ${product.createdAt}
-
-<b>🏷 ${product.topics}</b>
-`;
+  return data.data.posts.nodes.map((post) => new Product(post));
 }
 
 async function sendTelegramResponse(chatId, message) {
@@ -365,6 +293,9 @@ async function sendTelegramMediaResponse(chatId, product, index) {
   const photo = product.media.length > 0 ? product.media[0].url : "";
 
   const apiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendPhoto`;
+ 
+
+  // const originalUrl = 
   const params = {
     chat_id: chatId,
     // media: { type: 'photo', media: photo },
@@ -373,7 +304,7 @@ async function sendTelegramMediaResponse(chatId, product, index) {
     caption: html,
     method: "post",
     reply_markup: {
-      inline_keyboard: [[{ text: "Go to website", url: product.url }]],
+      inline_keyboard: [[{ text: "Go to website", url: product.website }]],
     },
     parse_mode: "HTML",
   };
@@ -399,90 +330,83 @@ async function sendTelegramMediaResponse(chatId, product, index) {
   }
 }
 
+// Main functions
 async function sendToUser(chatId, timePeriod) {
-  const products = await fetchProductHuntData(chatId, timePeriod);
-  let index = 0;
+  const introMessage = getIntroMessage(timePeriod);
+  await sendTelegramResponse(chatId, introMessage);
 
-  console.log("length of products: ", products.length);
+  const products = await fetchProductHuntData(timePeriod);
+  for (let i = 0; i < products.length; i++) {
+    await sendTelegramMediaResponse(chatId, products[i], i);
+  }
+}
 
-  for (const product of products) {
-    await sendTelegramMediaResponse(chatId, product, index);
-    index++;
+function getIntroMessage(timePeriod) {
+  const messages = {
+    [TimePeriodEnum.TODAY]: "Today's products on Product Hunt:",
+    [TimePeriodEnum.YESTERDAY]: "Yesterday's products on Product Hunt:",
+    [TimePeriodEnum.THIS_WEEK]: "This week's products on Product Hunt:",
+    [TimePeriodEnum.THIS_MONTH]: "This month's products on Product Hunt:",
+    [TimePeriodEnum.LAST_WEEK]: "Last week's products on Product Hunt:",
+    [TimePeriodEnum.LAST_MONTH]: "Last month's products on Product Hunt:",
+  };
+  return messages[timePeriod] || "Products on Product Hunt:";
+}
+
+async function handleCommand(chatId, command) {
+  const commands = {
+    "/start": () => sendTelegramResponse(chatId, "Welcome to Product Hunt Bot! Use /today, /yesterday, /thisweek, /thismonth, /lastweek, /lastmonth to get the latest products on Product Hunt."),
+    "/help": () => sendTelegramResponse(chatId, "Use /today, /yesterday, /thisweek, /thismonth, /lastweek, /lastmonth to get the latest products on Product Hunt."),
+    "/contact": () => sendTelegramResponse(chatId, "You can contact me in telegram @noncain"),
+    "/today": () => sendToUser(chatId, TimePeriodEnum.TODAY),
+    "/yesterday": () => sendToUser(chatId, TimePeriodEnum.YESTERDAY),
+    "/thisweek": () => sendToUser(chatId, TimePeriodEnum.THIS_WEEK),
+    "/thismonth": () => sendToUser(chatId, TimePeriodEnum.THIS_MONTH),
+    "/lastweek": () => sendToUser(chatId, TimePeriodEnum.LAST_WEEK),
+    "/lastmonth": () => sendToUser(chatId, TimePeriodEnum.LAST_MONTH),
+  };
+
+  const handler = commands[command];
+  if (handler) {
+    await handler();
+  } else {
+    await sendTelegramResponse(chatId, "Invalid command. Please use /today, /yesterday, /thisweek, /thismonth, /lastweek, /lastmonth");
   }
 }
 
 async function handleRequest(request) {
   const { method, headers } = request;
-
   const url = new URL(request.url);
 
-  // Check if the request is a POST request to /webhooks/telegram and has JSON content-type
-  if (
-    method === "POST" &&
-    url.pathname == "/webhooks/telegram" &&
-    headers.get("content-type") === "application/json"
-  ) {
-    const data = await request.json();
-    const { message } = data;
 
+  if (method === "POST" && url.pathname === "/webhooks/telegram") {
+    const { message } = await request.json();
     if (message && message.text) {
       const command = message.text.trim();
       const chatId = message.chat.id;
-
-      const commands = [
-        "/today",
-        "/yesterday",
-        "/thisweek",
-        "/thismonth",
-        "/lastweek",
-        "/lastmonth",
-      ];
-      if (command === "/start") {
-        await sendTelegramResponse(
-          chatId,
-          "Welcome to Product Hunt Bot! Use /today, /yesterday, /thisweek, /thismonth, /lastweek, /lastmonth to get the latest products on Product Hunt."
-        );
-        return new Response("OK", { status: 200 });
-      }
-      // help
-      if (command === "/help") {
-        await sendTelegramResponse(
-          chatId,
-          "Use /today, /yesterday, /thisweek, /thismonth, /lastweek, /lastmonth to get the latest products on Product Hunt."
-        );
-        return new Response("OK", { status: 200 });
-      }
-      // contact
-      if (command === "/contact") {
-        await sendTelegramResponse(
-          chatId,
-          "You can contact me in telegram @noncain"
-        );
-        return new Response("OK", { status: 200 });
-      }
-      if (commands.includes(command)) {
-        await sendToUser(chatId, command.split("/").pop());
-        return new Response("OK", { status: 200 });
-      } else {
-        await sendTelegramResponse(
-          chatId,
-          "Invalid command. Please use /today, /yesterday, /thisweek, /thismonth, /lastweek, /lastmonth"
-        );
-        return new Response("OK", { status: 200 });
-      }
+      await handleCommand(chatId, command);
     }
   }
 
   return new Response("OK", { status: 200 });
 }
 
-// commands
-// start - Get start
-// today - Get today products
-// yesterday - Get yesterday products
-// thisweek - Get this week products
-// thismonth - Get this month products
-// lastweek - Get last week products
-// lastmonth - Get last month products
-// help - Get help message
-// contact - call me in telegram
+// Export
+export default {
+  async fetch(request, env, ctx) {
+    return handleRequest(request);
+  },
+
+  async scheduled(event, env, ctx) {
+    const cronJobs = {
+      "0 1 * * *": () => sendToUser(CHANNEL_ID, TimePeriodEnum.YESTERDAY),
+      "0 0 * * mon": () => sendToUser(CHANNEL_ID, TimePeriodEnum.LAST_WEEK),
+      "0 0 1 * *": () => sendToUser(CHANNEL_ID, TimePeriodEnum.LAST_MONTH),
+    };
+
+    const job = cronJobs[event.cron] || cronJobs["0 1 * * *"];
+    await job();
+
+    return new Response(event.cron + " OK", { status: 200 });
+  },
+};
